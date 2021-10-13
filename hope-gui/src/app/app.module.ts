c9ddccfa2773853177c661d434eff841d18ae0bf
Daniele <d.tarini@hyperborea.com>
2021-10-14 00:08:35 +0200

    Smartapp basics

--- hope-gui/src/app/app.module.ts
+++ hope-gui/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    //{ provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
